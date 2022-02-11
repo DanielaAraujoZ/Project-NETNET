7b815a90b224670e050a607d192980f3c31c8a3a
Daniela Araujo <daraujoz@unal.edu.co>
2022-02-11 16:57:36 -0500

    Se crea el servidor con JSON inicialmente, se agrega dinamismo a las peliculas y al boton de busqueda

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,63 @@
-import { dataMoviesAmime } from "../data/dataMoviesAnime.js";
-import { dataMoviesPopular } from '../data/dataMoviesPopular.js'
-import { dataMoviesEU } from '../data/dataMoviesEU.js'
+const baseURL = " http://localhost:4000/movies";
+const divPopular = document.getElementById("contentPopular");
+const divAnime = document.getElementById("contentAnime");
+const divCrimen = document.getElementById("contentCrimen");
 
-const divPopular = document.getElementById('contentPopular')
-const divAnime = document.getElementById('contentAnime')
-const divCrimen = document.getElementById('contentCrimen')
+async function getInfo(url) {
+  try {
+    const answer = await (await fetch(url)).json();
+    return answer;
+  } catch (error) {
+    console.log("Hay un error");
+  }
+}
 
-dataMoviesPopular.map((item) => {
-    divPopular.innerHTML += `
-    <img src="${item.img}" alt="${item.name}" class="movieItem" >
-    `
-})
+async function showMovies(toSearch) {
+  let data;
 
-dataMoviesAmime.map((item) => {
-    divAnime.innerHTML += `
-    <img src="${item.img}" alt="${item.name}" class="movieItem" >
-    `
-})
+  if (toSearch == undefined) {
+    data = await getInfo(baseURL);
+  } else {
+    data = toSearch;
+    divPopular.innerHTML = "";
+    divAnime.innerHTML = "";
+    divCrimen.innerHTML = "";
+  }
 
-dataMoviesEU.map((item) => {
-    divCrimen.innerHTML += `
-    <img src="${item.img}" alt="${item.name}" class="movieItem" >
-    `
-})
+  let popular = data.filter((item) => item.genre === "popular");
+  let anime = data.filter((item) => item.genre === "anime");
+  let crimen = data.filter((item) => item.genre === "crimen");
+
+  function sentToDiv(data, idDiv) {
+    data.map((item) => {
+      const { name, img, id } = item;
+
+      idDiv.innerHTML += `
+              <img src="${img}" alt="${name}" class="movieItem" onclick="sendMovie(${id})">   
+        `;
+    });
+  }
+  sentToDiv(popular, divPopular);
+  sentToDiv(anime, divAnime);
+  sentToDiv(crimen, divCrimen);
+}
+
+showMovies();
+
+async function sendMovie(idMovie) {
+  let data = await getInfo(baseURL);
+  let movieClick = data.filter((item) => item.id === idMovie);
+  localStorage.setItem("MOVIE", JSON.stringify(movieClick));
+  window.location.href = "./info.html";
+}
+
+const searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", async () => {
+  const inputSearch = document.getElementById("inputSearch").value;
+  let data = await getInfo(baseURL);
+  let dataFilter = data.filter((item) =>
+    item.name.toLowerCase().includes(inputSearch.toLowerCase())
+  );
+  showMovies(dataFilter);
+});
--- src/scripts/info.js
+++ src/scripts/info.js
@@ -1,9 +1,68 @@
-import { dataMoviesEU } from '../data/dataMoviesEU.js'
+const infoLocal = JSON.parse(localStorage.getItem("MOVIE"));
+console.log(infoLocal[0]);
+const baseURL = " http://localhost:4000/movies";
+const contentSimilar = document.getElementById("contentSimilar");
 
-const contentSimilar = document.getElementById('contentSimilar')
+async function getInfo(url) {
+  try {
+    const answer = await (await fetch(url)).json();
+    return answer;
+  } catch (error) {
+    console.log("Hay un error");
+  }
+}
 
-dataMoviesEU.map((item) => {
-    contentSimilar.innerHTML += `
-    <img src="${item.img}" alt="${item.name}" class="movieItem" >
-    `
-})
+async function showMovies(callback) {
+  let data = await callback;
+  let popular = data.filter((item) => item.genre === "popular");
+
+  popular.map((item) => {
+    contentSimilar.innerHTML += `<img src="${item.img}" alt="${item.name}" class="movieItem" >
+        `;
+  });
+}
+
+showMovies(getInfo(baseURL));
+
+//Mostrar pelicula relacionada
+const containerDiv = document.getElementById("contentMain");
+
+window.addEventListener("DOMContentLoaded", async () => {
+  const { name, img, year, duration, description, id } = infoLocal[0];
+  
+  containerDiv.innerHTML += `
+    <img class="imageMovie" src="${img}" alt="${name}"/>
+          <h1 class="titleMovie">${name}</h1>
+          <div class="contentData flex flex-row">
+            <p class="porcentaje">99% de coincidencia</p>
+            <div class="group flex flex-row">
+              <p>${year}</p>
+              <img src="../Images/imagesProject/r.png" />
+            </div>
+            <div class="group flex flex-row">
+              <p>${duration}</p>
+              <img src="../Images/imagesProject/ha.png" />
+            </div>
+          </div>
+          <div class="buttons">
+            <button class="buttonsOpt">
+              <img src="../Images/imagesProject/buttonPlay.png" />
+            </button>
+            <button class="buttonsOpt mt-2">
+              <img src="../Images/imagesProject/buttonDownload.svg" />
+            </button>
+          </div>
+          <p class="descriptionMovie">
+            ${description}
+          </p>
+          <div class="credits">
+            <p>Elenco: Keanu Reeves, Laurence Fishburne...<b>más</b></p>
+            <p class="mt-2">Dirigido por: Lilly Wachowski, Lana Wachowski</p>
+          </div>
+          <div class="buttonsGroup">
+            <button>
+              <img class="imageB" src="../Images/imagesProject/Frame.png" />
+            </button>
+          </div>
+    `;
+});
